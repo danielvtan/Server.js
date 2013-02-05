553ebd6ce6ae256519ae29ad2be824008079f75b
Daniel Tan <danielvtan@gmail.com>
2013-02-05 18:56:20 +0800

    Created client server library

--- client/Client/Source/Client.hx
+++ client/Client/Source/Client.hx
@@ -4,82 +4,29 @@ import flash.display.Sprite;
 import flash.Lib;
 
 import flash.events.Event;
-import flash.events.IOErrorEvent;
-import flash.events.ErrorEvent;
-import flash.events.ProgressEvent;
 
-import flash.net.Socket;
-import flash.net.XMLSocket;
-
-import flash.external.ExternalInterface;
+import server.Server;
 
 class Client extends Sprite {
 	
-	
-	private var socket:Socket;
+	private var server:Server;
+	static function main () {
+		Lib.current.addChild (new Client ());	
+	}
 	public function new () {
-		console("start");
+		trace("start");
 		super ();
 		this.addEventListener(Event.ADDED_TO_STAGE, onAdded);
 		
 	}
-	static function main () {
-		
-		Lib.current.addChild (new Client ());	
-	}
-	
 	private function onAdded(e:Event):Void {
 		init();
 	}
 	private function init():Void {
-		initSocket();
-		
-		connectSocket("127.0.0.1", 8080);
+		initServer();
 	}
-	private function initSocket():Void {
-		socket = new Socket();
-		socket.addEventListener(Event.CONNECT, onConnection); 
-	    socket.addEventListener(Event.CLOSE, onClose); 
-	    socket.addEventListener(ErrorEvent.ERROR, onError); 
-	    socket.addEventListener(IOErrorEvent.IO_ERROR, onIOError); 
-	    socket.addEventListener(ProgressEvent.SOCKET_DATA, onData); 
-	    
-	}
-	private function connectSocket(host:String, port:Int):Void {
-		socket.connect(host, port);
-	}
-	private function onConnection(e:Event):Void {
-		console("connected");
-		
-		//socket.writeUTF("test");
-		socket.writeUTFBytes("test");
-		socket.flush();
-	}
-	private function onClose(e:Event):Void {
-		console("closed");
-		socket.close();
-	}
-	private function onError(e:ErrorEvent):Void {
-		console(e.errorID + " " + e.text);
-	}
-	private function onIOError(e:IOErrorEvent):Void {
-		console("onIOError");
-	}
-	private function onData(e:ProgressEvent):Void {
-		console("onData");
-		console("reading readUTFBytes-> " + socket.readUTFBytes(socket.bytesAvailable));
-	}
-	
-	private function console(e:String):Void {
-		try {
-			//if(ExternalInterface.available)
-			//	ExternalInterface.call("function(){" + e + "}");
-			//else
-				trace(e);
-		} catch(e:Dynamic) {
-			trace("Error " + e);
-		}
-		
+	private function initServer():Void {
+		server = new Server("127.0.0.1", 8080);
 	}
 
 }
---
+++ client/Client/Source/server/Server.hx
@@ -0,0 +1,54 @@
+package server;
+
+import flash.events.EventDispatcher;
+import flash.events.Event;
+import flash.events.ErrorEvent;
+import flash.events.IOErrorEvent;
+import flash.events.ProgressEvent;
+
+import flash.net.Socket;
+
+class Server extends EventDispatcher {
+	
+	private var socket:Socket;
+	private var host:String;
+	private var port:Int;
+	
+	public function new (h:String, p:Int) {
+		super();
+
+		host = h;
+		port = p;
+		initSocket();
+	}		
+	private function initSocket():Void {
+		socket = new Socket(host, port);
+		socket.addEventListener(Event.CONNECT, onConnection); 
+	    socket.addEventListener(Event.CLOSE, onClose); 
+	    socket.addEventListener(ErrorEvent.ERROR, onError); 
+	    socket.addEventListener(IOErrorEvent.IO_ERROR, onIOError); 
+	    socket.addEventListener(ProgressEvent.SOCKET_DATA, onData); 
+	    
+	}
+	private function onConnection(e:Event):Void {
+		trace("connected");
+		
+		//socket.writeUTF("test");
+		socket.writeUTFBytes("test");
+		socket.flush();
+	}
+	private function onClose(e:Event):Void {
+		trace("closed");
+		socket.close();
+	}
+	private function onError(e:ErrorEvent):Void {
+		trace(e.errorID + " " + e.text);
+	}
+	private function onIOError(e:IOErrorEvent):Void {
+		trace("onIOError");
+	}
+	private function onData(e:ProgressEvent):Void {
+		trace("onData");
+		trace("reading readUTFBytes-> " + socket.readUTFBytes(socket.bytesAvailable));
+	}
+}
